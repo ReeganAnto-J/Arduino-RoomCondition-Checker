8e389cee789649e2f279fe76351347d592bc9e97
Dinesh J <jd2675081@gmail.com>
2024-06-03 21:46:21 +0530

    changed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SensorDashboard from './SensorDashboard';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SensorDashboard />
+    </div>
+  );
+}
+
+export default App;
